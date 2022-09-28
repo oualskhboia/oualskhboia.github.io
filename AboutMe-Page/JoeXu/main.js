bbe7e6714f9717d1f770e38f94193b3ae74d3e28
oualskhboia <114541733+oualskhboia@users.noreply.github.com>
2022-09-28 11:46:41 +1000

    update subimit

--- AboutMe-Page/JoeXu/main.js
+++
@@ -1,8 +0,0 @@
-function hambergerMenu() {
-  var x = document.getElementById("myTopNavBar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
